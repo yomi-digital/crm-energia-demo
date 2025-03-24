fa5a2a6fdbdb74243c2ab5417638de11dd6590ac
yomidigital <87752809+yomidigital@users.noreply.github.com>
2025-03-24 22:28:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [// Docs: https://github.com/posva/unplugin-vue-router
   // ℹ️ This plugin should be placed before vue plugin
     VueRouter({
